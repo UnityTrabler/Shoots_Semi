31542d0ccf8e6019b562f78a30a0c02fffe12dba
wrqbin <leobin97@gmail.com>
2024-11-13 17:37:36 +0900

    오류수정

--- Shoots/src/main/java/net/inquiryComment/action/InquiryCommentFrontController.java
+++ Shoots/src/main/java/net/inquiryComment/action/InquiryCommentFrontController.java
@@ -7,7 +7,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import net.comment.action.CommentAddAction;
 import net.core.Action;
 import net.core.ActionForward;
 
