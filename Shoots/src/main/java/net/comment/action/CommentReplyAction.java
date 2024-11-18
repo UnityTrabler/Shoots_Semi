e40b4034a16e2d310ce239069dc68557426b19ed
wrqbin <leobin97@gmail.com>
2024-11-18 20:16:55 +0900

    댓글 하는중

--- Shoots/src/main/java/net/comment/action/CommentReplyAction.java
+++ Shoots/src/main/java/net/comment/action/CommentReplyAction.java
@@ -5,15 +5,26 @@ import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import net.comment.db.CommentBean;
+import net.comment.db.CommentDAO;
 import net.core.Action;
 import net.core.ActionForward;
 
 public class CommentReplyAction implements Action {
 
 	@Override
-	public ActionForward execute(HttpServletRequest req, HttpServletResponse resp)
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		CommentDAO dao = new CommentDAO();
+		
+		CommentBean co = new CommentBean();
+		co.setWriter(Integer.parseInt(request.getParameter("writer")));
+		co.setContent(request.getParameter("content"));
+		co.setComment_ref_id (Integer.parseInt(request.getParameter("comment_ref_id"))); 
+		co.setPost_id (Integer.parseInt(request.getParameter("post_id")));
+		
+		int ok = dao.commentsReply(co);
+		response.getWriter().print(ok);
 		return null;
 	}
 
--- Shoots/src/main/java/net/post/action/PostListAction.java
+++ Shoots/src/main/java/net/post/action/PostListAction.java
@@ -38,6 +38,7 @@ public class PostListAction implements Action {
 		request.getSession().setAttribute("referer", "list");
 		request.getSession().setAttribute("postlist", "list");
 		request.getSession().setAttribute("category", category);
+		
 		// 로그인 성공시 파라미터 page가 없어요. 그래서 초기값이 필요합니다.
 		int page = 1; // 보여줄 page
 		int limit = 10; // 한 페이지에 보여줄 게시판 목록의 수
